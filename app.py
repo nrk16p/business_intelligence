cae3f302899957dbf524e63bca3365d31c21e4ef
(670108) ณรงค์กรณ์ อัจฉริยญาติ <hardman16p@gmail.com>
2025-09-18 21:26:28 +0700

    Initial commit - Flask app with Jenkinsfile

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello from Flask + Jenkins CI/CD 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5011, debug=True)
